6fe2d1ea443e379c983a0de9fa391a70b8e2074d
WalleBot9 <mihirmu@outlook.com>
2020-06-23 11:49:12 -0700

    Added WASD.

--- NinjaEscape/Assets/Scripts/PlayerController.cs
+++ NinjaEscape/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 5;
+    TouchController touch;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,5 +14,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
     }
 }
