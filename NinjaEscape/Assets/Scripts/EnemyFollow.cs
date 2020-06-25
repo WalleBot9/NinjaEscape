6e9ccc076d2077fde136004b74416bdd0407f4ef
TheChikGoesBok <67165700+TheChikGoesBok@users.noreply.github.com>
2020-06-25 10:27:44 -0700

    Making Enemy
    Made part of the enemy

---
+++ NinjaEscape/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public GameObject player;
+    float speed = 4;
+    float radius = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+}
